20b9bf3b8dcf95d0aefad91639b1ed9758d00a1c
Darya <dashakomiss37@gmail.com>
2023-07-05 21:01:57 +0300

    add inf in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import './Footer.css';
+
 export const Footer = () => {
   return (
     <footer className="footer">
-   Footer
+      <div className="footer__block">
+        <h2 className="footer__block__title">Poster</h2>
+        <div className="footer__block__item">Now in the cinema</div>
+        <div className="footer__block__item">For children</div>
+      </div>
+      <div className="footer__block">
+        <h2 className="footer__block__title">Cinema</h2>
+        <div className="footer__block__item">News</div>
+        <div className="footer__block__item">Vacancies</div>
+        <div className="footer__block__item">Contacts</div>
+      </div>
+      <div className="footer__block">
+        <h2 className="footer__block__title">Info</h2>
+        <div className="footer__block__item">Rules</div>
+        <div className="footer__block__item">Addresses</div>
+        <div className="footer__block__item">Privacy</div>
+      </div>
     </footer>
   );
 };
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Footer} from '../../components/Footer';
+import {Films} from "../../components/Films";
 
 export const MainPage = () => {
   return (
     <>
+      <Films />
       <Footer/>
     </>
   );
