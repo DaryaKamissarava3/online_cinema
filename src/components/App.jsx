7ba3031a27913a4ca13a7ab9b3ecdef3d1ca4a3d
Darya <dashakomiss37@gmail.com>
2023-08-01 09:47:24 +0300

    remove navigation component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,20 +5,19 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 
-import { Navigation } from './SharedComponents/Navigation';
-import { AdminPage } from '../pages/AdminPage';
-import { Login } from '../pages/Login';
-import { Registration } from '../pages/Registration';
-import { MainPage } from './UserComponents/MainPage';
-import { AddFilms } from '../pages/AdminPage/AdminComponents/AddFilms';
-import { ViewUsers } from '../pages/AdminPage/AdminComponents/ViewUsers';
-import { Films } from './SharedComponents/Films';
-import { FilmDetails } from './SharedComponents/FilmDetails';
-import { UserTickets } from './UserComponents/UserTickets';
-import { UserAccount } from './UserComponents/UserAccount';
-import { About } from '../pages/About';
+import {AdminPage} from '../pages/AdminPage';
+import {Login} from '../pages/Login';
+import {Registration} from '../pages/Registration';
+import {MainPage} from './UserComponents/MainPage';
+import {AddFilms} from '../pages/AdminPage/AdminComponents/AddFilms';
+import {ViewUsers} from '../pages/AdminPage/AdminComponents/ViewUsers';
+import {Films} from './SharedComponents/Films';
+import {FilmDetails} from './SharedComponents/FilmDetails';
+import {UserTickets} from './UserComponents/UserTickets';
+import {UserAccount} from './UserComponents/UserAccount';
+import {About} from '../pages/About';
 
 function App() {
   const loggedOut = useSelector((state) => state.auth.isLoggedIn !== true);
@@ -27,34 +26,33 @@ function App() {
   return (
     <BrowserRouter>
       <div className="app">
-        <Navigation />
-          <Routes>
-            { loggedOut  ? (
-              <>
-                <Route path="/registration" element={<Registration />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<Navigate to="/login" replace={true} />} />
-              </>
-            ) : userRole === 'admin' ? (
-              <>
-                <Route path="*" element={<Navigate to="/admin" replace={true} />} />
-                <Route path="/admin" element={<AdminPage />} />
-                <Route path="/admin/add-films" element={<AddFilms />} />
-                <Route path="/admin/view-users" element={<ViewUsers />} />
-                <Route path="/admin/films" element={<Films />} />
-                <Route path="films/:id" element={<FilmDetails />} />
-              </>
-            ) : (
-              <>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/tickets" element={<UserTickets />} />
-                <Route path="/profile" element={<UserAccount />} />
-                <Route path="/about" element={<About />} />
-                <Route path="films/:id" element={<FilmDetails />} />
-                <Route path="*" element={<Navigate to="/" replace={true} />} />
-              </>
-            )}
-          </Routes>
+        <Routes>
+          {loggedOut ? (
+            <>
+              <Route path="/registration" element={<Registration/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="*" element={<Navigate to="/login" replace={true}/>}/>
+            </>
+          ) : userRole === 'admin' ? (
+            <>
+              <Route path="*" element={<Navigate to="/admin" replace={true}/>}/>
+              <Route path="/admin" element={<AdminPage/>}/>
+              <Route path="/admin/add-films" element={<AddFilms/>}/>
+              <Route path="/admin/view-users" element={<ViewUsers/>}/>
+              <Route path="/admin/films" element={<Films/>}/>
+              <Route path="films/:id" element={<FilmDetails/>}/>
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<MainPage/>}/>
+              <Route path="/tickets" element={<UserTickets/>}/>
+              <Route path="/profile" element={<UserAccount/>}/>
+              <Route path="/about" element={<About/>}/>
+              <Route path="films/:id" element={<FilmDetails/>}/>
+              <Route path="*" element={<Navigate to="/" replace={true}/>}/>
+            </>
+          )}
+        </Routes>
       </div>
     </BrowserRouter>
   );
