ee2717be7b0d4db43e3b1f431b4ef9a93eed84a9
Darya <dashakomiss37@gmail.com>
2023-08-09 18:54:30 +0300

    change footer using material ui

--- src/components/SharedComponents/Footer/Footer.jsx
+++ src/components/SharedComponents/Footer/Footer.jsx
@@ -1,27 +1,63 @@
 import React from 'react';
 
-import './Footer.css';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+import {makeStyles} from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  footerIcons: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  copyright: {
+    marginTop: theme.spacing(2),
+    textAlign: 'center',
+  },
+}));
+
+const footerTitles = ['PROFILE', 'RECENT POSTS', 'CUSTOMER'];
+const footerProfile = ['FAQs', 'Pricing plans', 'Order tracking', 'Returns'];
+const footerRecentPosts = ['Touch of uniqueness', 'Offices you won’t forget', 'Cicilan'];
+const footerCustomer = ['Help & contact us', 'Return', 'Online stores', 'Terms & cordition'];
 
 export const Footer = () => {
+  const classes = useStyles();
   return (
-    <footer className="footer">
-      <div className="footer__block">
-        <h2 className="footer__block__title">Poster</h2>
-        <div className="footer__block__item">Now in the cinema</div>
-        <div className="footer__block__item">For children</div>
-      </div>
-      <div className="footer__block">
-        <h2 className="footer__block__title">Cinema</h2>
-        <div className="footer__block__item">News</div>
-        <div className="footer__block__item">Vacancies</div>
-        <div className="footer__block__item">Contacts</div>
-      </div>
-      <div className="footer__block">
-        <h2 className="footer__block__title">Info</h2>
-        <div className="footer__block__item">Rules</div>
-        <div className="footer__block__item">Addresses</div>
-        <div className="footer__block__item">Privacy</div>
-      </div>
-    </footer>
+    <>
+      <Box ml={18} mt={12} mb={4}>
+        <Grid container spacing={2} justifyContent="center">
+          {footerTitles.map((title, index) => (
+            <Grid item key={index} xs={12} sm={4}>
+              <h4>{title}</h4>
+              {index === 0 && footerProfile.map((item, subIndex) => <p key={subIndex}>{item}</p>)}
+              {index === 1 && footerRecentPosts.map((item, subIndex) => <p key={subIndex}>{item}</p>)}
+              {index === 2 && footerCustomer.map((item, subIndex) => <p key={subIndex}>{item}</p>)}
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+      <Box className={classes.footerIcons}>
+        <Link href='https://www.instagram.com/' mr={3}>
+          <InstagramIcon/>
+        </Link>
+        <Link href='https://www.facebook.com/' mr={3}>
+          <FacebookIcon/>
+        </Link>
+        <Link href='https://twitter.com/' mr={3}>
+          <TwitterIcon/>
+        </Link>
+      </Box>
+      <Box className={classes.copyright} mb={8}>
+        &copy; 2023 Your Company Name. All rights reserved.
+      </Box>
+    </>
   );
 };
