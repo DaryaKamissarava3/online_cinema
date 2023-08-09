bae1d3899407e431cdb16d6708a65d15a488cbb9
Darya <dashakomiss37@gmail.com>
2023-08-09 11:35:11 +0300

    add badge for tickets

--- src/components/UserComponents/UserMenu/UserMenu.jsx
+++ src/components/UserComponents/UserMenu/UserMenu.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import Box from '@mui/material/Box';
-import {Breadcrumbs} from '@mui/material';
+import Badge from '@mui/material/Badge';
+import { Breadcrumbs } from '@mui/material';
 import Link from '@mui/material/Link';
 
 import {makeStyles} from '@mui/styles';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    margin:theme.spacing(5),
+    margin: theme.spacing(5),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -20,23 +21,25 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     padding: theme.spacing(3),
-    borderRadius:'20px',
+    borderRadius: '20px',
   },
 }));
 
 export const UserMenu = () => {
-  const classes=useStyles();
+  const classes = useStyles();
 
   return (
     <Box className={classes.container}>
-      <Breadcrumbs  className={classes.breadcrumbs}>
-        <Link underline="hover" href="/tickets" sx={{color:'white'}} >
-          My tickets
-        </Link>
-        <Link underline="hover" href="/account" sx={{color:'white'}} >
+      <Breadcrumbs className={classes.breadcrumbs}>
+        <Badge badgeContent={4} color="secondary" showZero max={10}>
+          <Link underline="hover" href="/tickets" sx={{color: 'white'}} mr={1}>
+            My tickets
+          </Link>
+        </Badge>
+        <Link underline="hover" href="/account" sx={{color: 'white'}}>
           Account
         </Link>
-        <Link underline="hover" href="/about" sx={{color:'white'}} >
+        <Link underline="hover" href="/about" sx={{color: 'white'}}>
           About cinema
         </Link>
       </Breadcrumbs>
